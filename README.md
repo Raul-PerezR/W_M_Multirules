27cace279f9ccd2b155a86b6d24ef015d510a8ba
Raul-PerezR <fgr@decsai.ugr.es>
2023-02-09 11:53:55 +0100

    update

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ Parameters Basic:
 
 Learning Parameters: 
 * `-LearningModel <num>` or `-LM <num>` to select the learning model:
-* * `1`	Classic Chi Algorithm. Selected by default
+* * `1`	Basic QChi Algorithm. Selected by default
 * * `2`	Amount version. More rules than the central are considered. In this case, a number fixes of rules for each example, following a heuristic, is included. 
 * * `3`	Threshold version. More rules than the central are considered. A threshold on the adaptation of the central rule is fixed expressed by a pertantage. All the rule with with adaptation igual or greather than that threshold are include in the rule set.
 * * `4`	Normalized Threshold version. More rules than the central are considered. Similar to the previous model but using the normalized adaptation of the central rule.
@@ -34,12 +34,20 @@ Learning Parameters:
 * `-ld <num>` being `<num>` the value of the maximum hamming distance permitted. By default, ld = 0
 * `-size <num>` or `-sz <num>` establishes the maximum number of rules in the final rule set. 0 meaning not limited. By default, size = 0
 * `-NotInferenceTraining` or `-Nit` does not apply real inference on training set (it is an estimation obtained from the learning process). By default not included
+* `-weightCalculateProcess` or `-wCP` (by default is off, that is, it is calculated by quick new version)
 * `-NormalizedMu` uses the normalized adaptation degrees (by default the normalization is not used).
 * `-weightCalculateProcess <num>` or `-wCP <num>` activate exhaustive traditional computation for obtaining the weight of the rule.
-* `-weightRuleModel <num>` or `-w <num>` define how rule's weight is calculated by the Chi algorithm:
+* `-allContinuous` actived considers all variables (nominals included) like continuous (by default this parameter is false)
+* `-weightRuleModel <num>` or `-w <num>` define how rule's weight is calculated by the QChi algorithm:
+* * `0` All rules with weight = 1
 * * `1`	PCF (by default)
 * * `2`	NSLV model 
 * * `3`	Original Chi Strategy 2
+* * `4` PCF-CS
+* `-acc <num>` define the measurement of the performance of the algorithm:
+* * `0` Accuracy (by default)
+* * `1` No defined
+* * `2` Geometric Mean (GM)
 
 Rule Filtering Parameters: 
 *	 `-RuleFilteringMethod <num>` or `-RF <num>` to select the filtering rule process:
@@ -60,16 +68,17 @@ Inference Parameters:
 * `-PerCentOnTest <real_num>` establishes de percentage of the examples from the test set on which the inference is applied. By default `PerCentOnTest 1.0`
 
 
+
 ## Some examples
 
-##### ./RedundancyStudyChi -e ../data/texture/texture -nlabel 5
+##### ./W_M_Multirules -e ../data/texture/texture -nlabel 5
 Result of 10 cross-validation on the *texture* database, learning with *Classic CHI algorithm* and infering with *Heuristic Neighborhood Inference* taking 5 uniformly distributed labels cutoff at 0.5 over the universe of discourse of continuous variables.
 
-##### ./RedundancyStudyChi -e ../data/census/census -IM 4 -nlabel 5 -maxrules 1024
+##### ./W_M_Multirules -e ../data/census/census -IM 4 -nlabel 5 -maxrules 1024
 Result of 10 cross-validation on the *census* database learning with *Classic CHI algorithm* and infering with *Heuristic Neighborhood Inference* taking 5 uniformly distributed labels and setting the inference parameter on the maximum number of rule to search in the example neighbor to 2^10 = 1024.
 
 
-##### ./RedundancyStudyChi -e ../data/satimage/satimage -LM 2 -times 5 -nlabel 3 -maxrules 1024 -size 1000000 
+##### ./W_M_Multirules -e ../data/satimage/satimage -LM 2 -times 5 -nlabel 3 -maxrules 1024 -size 1000000 
 Result of 10 cross-validation on the *satimage* database learning with *Amount Version of CHI algorithm*, considering 5 antecedents for each example, infering with *Heuristic Neighborhood Inference* taking 5 uniformly distributed labels and setting the inference parameter on the maximum number of rule to search in the example neighbor to 2^10 = 1024, and it is not permitted more than 1000000 rules in the final rule set.
 
 # W_M_Multirules
--- src/launch_fie3.sh
+++ src/launch_fie3.sh
@@ -1,61 +1,62 @@
-echo "===================== parameter experiment: -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    " >>./QckChi_result_5labels.csv
+echo "===================== parameter experiment: -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    " >>./QckChi_result_5labels.csv
 echo "Database Lab #Cont Global #Ex #Rules #Train #ErrorTrain #Test #TestWNC #Error %NC TrainTime Nose TestTime"  >> ./QckChi_result_5labels.csv
-./W_M_Multirules -e ../databases/abalone/abalone -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/adult/adult -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/australian/australian -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/banana/banana -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/bands/bands -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/bupa/bupa -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/census/census -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/cleveland/cleveland -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/coil2000/coil2000 -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/covtype_2_vs_1/covtype_2_vs_1 -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/crx/crx -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/fars_0_vs_4_/fars_0_vs_4_ -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/heart/heart -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/iris/iris -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/letter/letter -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/magic/magic -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/newthyroid/newthyroid -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/mammographic/mammographic -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/page-blocks/page-blocks -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/penbased/penbased -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/phoneme/phoneme -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/pima/pima -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/ring/ring -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/saheart/saheart -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/satimage/satimage -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/segment/segment -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/shuttle/shuttle -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/spambase/spambase -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/texture/texture -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/thyroid/thyroid -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/twonorm/twonorm -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/vehicle/vehicle -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/vowel/vowel -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/winequality-red/winequality-red -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-./W_M_Multirules -e ../databases/winequality-white/winequality-white -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv  
+./W_M_Multirules -e ../databases/abalone/abalone -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/adult/adult -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/australian/australian -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/banana/banana -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/bands/bands -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/bupa/bupa -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/census/census -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/cleveland/cleveland -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/coil2000/coil2000 -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/covtype_2_vs_1/covtype_2_vs_1 -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/crx/crx -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/fars_0_vs_4_/fars_0_vs_4_ -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/heart/heart -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/iris/iris -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/letter/letter -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/magic/magic -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/newthyroid/newthyroid -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/mammographic/mammographic -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/page-blocks/page-blocks -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/penbased/penbased -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/phoneme/phoneme -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/pima/pima -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/ring/ring -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/saheart/saheart -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/satimage/satimage -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/segment/segment -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/shuttle/shuttle -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/spambase/spambase -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/texture/texture -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/thyroid/thyroid -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/twonorm/twonorm -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/vehicle/vehicle -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/vowel/vowel -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/winequality-red/winequality-red -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+./W_M_Multirules -e ../databases/winequality-white/winequality-white -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv  
 echo "===========================================================================================================================" >> ./QckChi_result_7labels.csv
 
+#No_Injury,Possible_Injury,Nonincapaciting_Evident_Injury,Incapaciting_Injury,Fatal_Injury,Injured_Severity_Unknown,Died_Prior_to_Accident,Unknown
 
-#./W_M_Multirules -e ../databases/banana/banana -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-#./W_M_Multirules -e ../databases/iris/iris -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-#./W_M_Multirules -e ../databases/led7digit/led7digit -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-#./W_M_Multirules -e ../databases/newthyroid/newthyroid -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-#./W_M_Multirules -e ../databases/page-blocks/page-blocks -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-#./W_M_Multirules -e ../databases/pima/pima -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-#./W_M_Multirules -e ../databases/shuttle/shuttle -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-#./W_M_Multirules -e ../databases/adult/adult -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-#./W_M_Multirules -e ../databases/census/census -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-#./W_M_Multirules -e ../databases/coil2000/coil2000 -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-#./W_M_Multirules -e ../databases/covtype_2_vs_1/covtype_2_vs_1 -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-#./W_M_Multirules -e ../databases/german/german -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-#./W_M_Multirules -e ../databases/magic/magic -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-#./W_M_Multirules -e ../databases/thyroid/thyroid -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-#./W_M_Multirules -e ../databases/sonar/sonar -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-#./W_M_Multirules -e ../databases/spectfheart/spectfheart -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-#./W_M_Multirules -e ../databases/segment/segment -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-#./W_M_Multirules -e ../databases/spambase/spambase -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-#./W_M_Multirules -e ../databases/satimage/satimage -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-#./W_M_Multirules -e ../databases/texture/texture -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
-#./W_M_Multirules -e ../databases/letter/letter -nlabel 5 -LM 1 -IM 4 -Nit   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/banana/banana -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/iris/iris -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/led7digit/led7digit -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/newthyroid/newthyroid -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/page-blocks/page-blocks -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/pima/pima -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/shuttle/shuttle -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/adult/adult -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/census/census -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/coil2000/coil2000 -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/covtype_2_vs_1/covtype_2_vs_1 -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/german/german -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/magic/magic -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/thyroid/thyroid -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/sonar/sonar -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/spectfheart/spectfheart -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/segment/segment -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/spambase/spambase -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/satimage/satimage -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/texture/texture -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
+#./W_M_Multirules -e ../databases/letter/letter -nlabel 5 -LM 1 -IM 4 -Nit -wCP   -O ./QckChi_result_5labels.csv    
